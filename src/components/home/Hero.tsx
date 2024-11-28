268c5b520627748e5e4ba61760e0ea874f07d727
VICTORMWANGI-ENG <mwangizeus69@gmail.com>
2024-11-28 08:43:55 +0300

    Fixes incorrect API endpoint
    Corrects the API endpoint for fetching job data.

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
 		const fetchJobs = async () => {
 			try {
 				const response = await fetch(
-					"https://medrin-jobs-backend-nn38.onrender.comcalhost:5000/job"
+					"https://medrin-jobs-backend-nn38.onrender.com/job"
 				);
 				const data = await response.json();
 				setJobsAmount(data.length || []);
